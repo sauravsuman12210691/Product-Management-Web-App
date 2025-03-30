e9c7bc9aafd40d3044864476893ac4a00dc10bfd
Saurav Suman <sauravsrivastav9876@gmail.com>
2025-03-30 10:03:21 +0530

    vercel.json added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
     <Router>
       <Routes>
         <Route path="/register" element={<Register />} />
-        <Route path="/login" element={<Login />} />
+        <Route path="/" element={<Login />} />
         <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
       </Routes>
     </Router>
